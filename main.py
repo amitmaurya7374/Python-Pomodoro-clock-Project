16a567e2302eb74387218a08a59d850b6258767d
amitmaurya7374 <amitmaurya7374@gmail.com>
2021-02-20 10:10:21 +0530

    created a canvas and added image.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -16,3 +17,18 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+# creating a starting window
+window = Tk()
+window.title("Pomodoro clock")
+window.config(padx=100, pady=50)
+# creating a canvas
+"""
+so what is canvas ?
+Canvas is a tkinter widget that allow us to place a things on top of each other.
+"""
+canvas = Canvas(height=224, width=200)  # our image is 200X223 pixel that why we take this value
+tomato_img = PhotoImage(file="tomato.png")  # fetch an image
+canvas.create_image(100, 112,
+                    image=tomato_img)  # to place an image in center of scren we will half a height and half of width
+canvas.pack()  # show a image on screen
+window.mainloop()
