436b09118a40320049e7108d9513369cafb8a7e9
amitmaurya7374 <amitmaurya7374@gmail.com>
2021-02-20 10:55:07 +0530

    completed an entire interface

--- main.py
+++ main.py
@@ -21,6 +21,10 @@ LONG_BREAK_MIN = 20
 window = Tk()
 window.title("Pomodoro clock")
 window.config(padx=100, pady=50, bg=YELLOW)
+# creating a label .
+timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 40, "bold"))
+timer_label.grid(row=0, column=1)
+
 # creating a canvas
 """
 so what is canvas ?
@@ -32,6 +36,16 @@ tomato_img = PhotoImage(file="tomato.png")  # fetch an image
 canvas.create_image(100, 112,
                     image=tomato_img)  # to place an image in center of scren we will half a height and half of width
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()  # show a image on screen
+canvas.grid(row=1, column=1)  # show a image on screen
 
+# Creating a button
+start_button = Button(text="start",bg=YELLOW)
+start_button.grid(row=2, column=0)
+
+# Creating a checkmark
+check_label = Label(text="✓", font=(FONT_NAME, 18), fg=GREEN, bg=YELLOW)
+check_label.grid(row=3, column=1)
+# reset button
+reset_button = Button(text="reset",bg=YELLOW)
+reset_button.grid(row=2, column=2)
 window.mainloop()
