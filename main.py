a99586e654171c5f7f9b1f08b0ef2a030fdca53e
amitmaurya7374 <amitmaurya7374@gmail.com>
2021-02-20 13:11:32 +0530

    added timer functionality

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
@@ -9,18 +10,65 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    """Start a timer"""
+    global reps
+    reps += 1
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break = LONG_BREAK_MIN * 60
+    if reps % 8 == 0:
+        count_down(long_break)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+    else:
+        count_down(work_sec)
+
+
+def reset_timer():
+    """Reset a timer to 00:00"""
+    canvas.itemconfig(timer_text, text="00:00")
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    """Start a count down"""
+    count_min = math.floor(count / 60)  # min
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+    else:
+        start_timer()
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 # creating a starting window
 window = Tk()
 window.title("Pomodoro clock")
 window.config(padx=100, pady=50, bg=YELLOW)
+# After method for ex:
+# def something(*args):
+#     for n in args:
+#         print(n)
+
+#
+# """
+# syntax for after method
+# window.after(seconds_in_ms,function_name,unlimited_positional_agrs_pass_to_that_function)
+# """
+# window.after(1000, something, 2, 3, 4, 5, 6)  # this after method will run after certain delay
+
 # creating a label .
 timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 40, "bold"))
 timer_label.grid(row=0, column=1)
@@ -35,17 +83,17 @@ canvas = Canvas(height=224, width=200, bg=YELLOW,
 tomato_img = PhotoImage(file="tomato.png")  # fetch an image
 canvas.create_image(100, 112,
                     image=tomato_img)  # to place an image in center of scren we will half a height and half of width
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(row=1, column=1)  # show a image on screen
 
 # Creating a button
-start_button = Button(text="start",bg=YELLOW)
+start_button = Button(text="start", bg=YELLOW, command=start_timer)
 start_button.grid(row=2, column=0)
 
 # Creating a checkmark
 check_label = Label(text="✓", font=(FONT_NAME, 18), fg=GREEN, bg=YELLOW)
 check_label.grid(row=3, column=1)
 # reset button
-reset_button = Button(text="reset",bg=YELLOW)
+reset_button = Button(text="reset", bg=YELLOW, command=reset_timer)
 reset_button.grid(row=2, column=2)
 window.mainloop()
